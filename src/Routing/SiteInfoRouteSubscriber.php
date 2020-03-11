e320c498482ece94e5bf4644635b2ff888144a18
Narendra Desai <shenvidesainarendra@gmail.com>
2020-03-11 16:43:53 +0530

    Axelerant: Added event route subscriber.

---
+++ src/Routing/SiteInfoRouteSubscriber.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\axe_site_api\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class SiteInfoRouteSubscriber
+ */
+class SiteInfoRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+
+  }
+}
