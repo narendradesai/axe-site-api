0949c427338b27997113ea899a91838f08b85de7
Narendra Desai <shenvidesainarendra@gmail.com>
2020-03-12 23:09:10 +0530

    Axelerant: Updated submit button text.

--- src/Form/SiteInfoForm.php
+++ src/Form/SiteInfoForm.php
@@ -3,12 +3,33 @@
 namespace Drupal\axe_site_api\Form;
 
 use Drupal\system\Form\SiteInformationForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\path_alias\AliasManagerInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Routing\RequestContext;
 
 /**
  * Class SiteInfoForm.
  */
 class SiteInfoForm extends SiteInformationForm {
+
+  public function __construct(ConfigFactoryInterface $config_factory, AliasManagerInterface $alias_manager, PathValidatorInterface $path_validator, RequestContext $request_context) {
+    parent::__construct($config_factory, $alias_manager, $path_validator, $request_context);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('path_alias.manager'),
+      $container->get('path.validator'),
+      $container->get('router.request_context')
+    );
+  }
 
   /**
    * {@inheritdoc}
